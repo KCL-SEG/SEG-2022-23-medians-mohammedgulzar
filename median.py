49f7a077cb8fd03db0a920c750cbb25b01942122
Mohammadgulzar Habibullah <mohammedgulzar79@gmail.com>
2023-09-24 14:11:33 +0100

    Completed Median Assignment

--- median.py
+++ median.py
@@ -1,12 +1,17 @@
 """Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
+        numbers = sorted([float(value) for value in input().split(",")])
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
+        if len(numbers) % 2 == 0:
+            upper_median = numbers[len(numbers) // 2]
+            lower_median = numbers[(len(numbers) // 2) - 1]
+            median = (upper_median + lower_median) / 2
+            print(f"Median value is {median}")
+        else:
+            print(f"Median value is {numbers[len(numbers) // 2]}")
         break
-print(numbers)
